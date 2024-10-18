0c9053400f35a0716dfecfb333448ce612f7f5e3
francisco <francisco.g1999@aluno.ifsc.edu.br>
2024-10-18 12:26:09 -0300

    progresso1

--- beecrowd10023.py
+++ beecrowd10023.py
@@ -1,7 +1,10 @@
 #coding: utf-8
+import math as math
 def obtencaoDados(i):
     #dados_por_inmovel = []
-    dados_do_inmovel = input(f"Ingresse o numero de pessoas seguido do consumo total do inmovel {i}: ").replace(" ","-")
+    dados_do_inmovel = input(f"Ingresse o numero de pessoas seguido do consumo total do inmovel {i+1}: ").split()
+    print(dados_do_inmovel)
+    dados_do_inmovel[0],dados_do_inmovel[1] = dados_do_inmovel[1],dados_do_inmovel[0]
     #numero_pessoas_no_inmovel = input(f"Entre com o numero de pessoas no inmovel {i+1}: ")
     #consumo_no_inmovel = input(f"Informe o consumo no inmovel {i+1}: ")
     #almacenamentoDados(dados_do_inmovel,dados_por_inmovel)
@@ -13,15 +16,14 @@ def almacenamentoDados(dado,memoria):
     return
 
 def consumoPessoaNoInmovel(dados):
-    return int(int(dados[1])/int(dados[0]))
+    return math.floor(int(dados[0])/int(dados[1]))
 
 def CalculoModificacaoDosDadosCidade(lista_com_dados):
     numero_cidades = len(lista_com_dados)
     for i in range(numero_cidades):
         numero_de_inmoveis_na_cidade = len(lista_com_dados[i])
         for j in range(numero_de_inmoveis_na_cidade):
-            dados = lista_com_dados[i][j].split("-")
-            lista_com_dados[i][j] = dados[0] + "-" + str(consumoPessoaNoInmovel(dados))
+            lista_com_dados[i][j][0] = consumoPessoaNoInmovel(lista_com_dados[i][j])
 
 
 
@@ -29,26 +31,22 @@ def CalculoModificacaoDosDadosCidade(lista_com_dados):
 
 if __name__ == "__main__":
     numero_inmoveis_na_cidade = int(input("Ingresse o numero de inmoveis na cidade: "))
-    dados_cidade = []
+    dados_cidades = []
     while numero_inmoveis_na_cidade !=0:
         dados_inmoveis_por_cidade = []
         for i in range(numero_inmoveis_na_cidade): 
             dados_recibidos_inmovel = obtencaoDados(i)
             dados_inmoveis_por_cidade.append(dados_recibidos_inmovel)
-        dados_cidade.append(dados_inmoveis_por_cidade)
+        dados_cidades.append(dados_inmoveis_por_cidade)
         numero_inmoveis_na_cidade = int(input("Ingresse o numero de inmoveis na cidade: "))
-    print(dados_cidade)
+    print(dados_cidades)
     
-    CalculoModificacaoDosDadosCidade(dados_cidade)
-    print(dados_cidade)
+    CalculoModificacaoDosDadosCidade(dados_cidades)
+    print(dados_cidades)
 
-    numero_da_cidade = len(dados_cidade) 
-    for i in range(1,numero_da_cidade+1):
+    numero_da_cidade = len(dados_cidades) 
+    for i in range(numero_da_cidade):
         num = 0
-        for j in range(len(dados_cidade[i])):
+        for j in range(len(dados_cidades[i])):
+            pass
             
-    
-        
-
-
-    
